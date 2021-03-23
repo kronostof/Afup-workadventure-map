756dfb8dca587abf6d7fb7986c2516ad3a647ea8
kronostof <christophe.moncy@gmail.com>
2021-03-23 06:04:53 +0000

    Deploying to gh-pages from master @ 67d8a92017d91017261ffb759f5f768c0994022a 🚀

---
+++ TS01_worldmap2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="TS01_worldmap2" tilewidth="32" tileheight="32" tilecount="60" columns="10">
+ <image source="TS01_worldmap2.bmp" width="320" height="200"/>
+</tileset>
